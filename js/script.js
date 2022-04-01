4a0c724e455baed8c9361b0a3570e448f0266814
Konrad Bauer <konradbauer94@gmail.com>
2022-04-01 18:49:33 +0200

    add remove task feature

--- js/script.js
+++ js/script.js
@@ -18,6 +18,12 @@
         render();
     };
 
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+
+        render();
+    };
+
     const render = () => {
 
         let htmlString = "";
@@ -27,12 +33,34 @@
         <li
             ${task.done ? " style=\"text-decoration: line-through\"" : ""}
         >
+            <button class="js-remove">Usuń zadanie</button>
             ${task.content}
         </li >
         `;
         }
 
-        document.querySelector(".js-tasks").innerHTML = htmlString.trim();
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-input").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        };
+
+        addNewTask(newTaskContent);
     };
 
     const init = () => {
@@ -40,19 +68,7 @@
         render();
 
         const form = document.querySelector(".js-form");
-
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-input").value.trim();
-
-            if (newTaskContent === "") {
-                return;
-            };
-
-            addNewTask(newTaskContent);
-        });
-
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
