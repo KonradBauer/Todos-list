9c4e81ffa237f1b14a4c2e45536faacb3c1a89c9
Konrad Bauer <konradbauer94@gmail.com>
2022-04-27 22:29:08 +0200

    added immutable task adding

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 {
-  const tasks = [];
+  let tasks = [];
 
   const addNewTask = (newTaskContent) => {
-    tasks.push({
-      content: newTaskContent,
-    });
+    tasks = [
+      ...tasks,
+      {
+        content: newTaskContent,
+      },
+    ];
+
     render();
   };
 
